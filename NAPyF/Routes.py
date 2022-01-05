9279990dcfd2a5d418d20dceb3ff224116ad31a8
greenfructose <justinclarkturney@gmail.com>
2022-01-05 13:40:37 -0600

    Fixed issue with route rebuilding after app deletion

--- NAPyF/Routes.py
+++ NAPyF/Routes.py
@@ -1,13 +1,14 @@
 from importlib import reload
 import autopep8
 from Settings import ROUTES_FILE, CODE_FORMAT_OPTIONS
-from ActiveApps import ACTIVE_APPS
+import ActiveApps
 
 
 def route_builder(global_static_directory, base_directory):
     """Builds routes based on active apps, removes inactive routes"""
     routes = []
-    for app in ACTIVE_APPS:
+    reload(ActiveApps)
+    for app in ActiveApps.ACTIVE_APPS:
         temp_app = app(global_static_directory, base_directory)
         for route in temp_app.routes:
             routes.append(route)
